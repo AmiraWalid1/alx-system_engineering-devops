e6a2a0a410237c7af65092113d7198bd39809d57
Amira Walid <amirawaleed012@gmail.com>
2024-04-25 18:44:50 +0200

    4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
 # set up client SSH configuration file with puppet.
-class { 'ssh':
-    client_options => {
-        'Host *' => {
-            'IdentityFile' => '~/.ssh/school',
-            'PasswordAuthentication' => 'no',
-        },
-    },
+file { '~/.ssh/config':
+    ensure  => 'file',
+    path    =>  '/etc/ssh/ssh_config',
+    content => "
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+    ",
 }
