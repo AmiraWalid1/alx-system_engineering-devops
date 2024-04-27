844bb1139afa759924a7e581fe74d5d638fe8dcf
Amira Walid <amirawaleed012@gmail.com>
2024-04-27 08:40:06 +0300

    update puppet file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,6 @@
 # set up client SSH configuration file with puppet.
 file { '~/.ssh/config':
     ensure  => 'file',
-    path    =>  '/etc/ssh/ssh_config',
     content => "
     IdentityFile ~/.ssh/school
     PasswordAuthentication no
