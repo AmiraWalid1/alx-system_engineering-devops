0c8bb74d2f4453cddb393770cc67e090e056a80f
Amira Walid <amirawaleed012@gmail.com>
2024-05-21 23:59:50 +0300

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,26 +9,24 @@ import sys
 
 if __name__ == "__main__":
     ''' main program '''
-    arg = sys.argv[1]
-    users_url = f'https://jsonplaceholder.typicode.com/users/{arg}'
-    response_user = requests.get(users_url)
+    usr_id = sys.argv[1]
+    url = f'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + f'users/{usr_id}')
 
-    if (response_user.status_code == 200):
-        user_name = json.loads(response_user.text)['name']
-        todo_url = f'https://jsonplaceholder.typicode.com/todos?userId={arg}'
-        response_todo = requests.get(todo_url)
+    if (user.status_code == 200):
+        user_name = user.json()['name']
+        todo = requests.get(url + f'todos?userId={usr_id}')
 
-        if response_todo.status_code == 200:
-            response_todo = json.loads(response_todo.text)
-            num_total_tasks = len(response_todo)
-            num_done_tasks = 0
+        if todo.status_code == 200:
+            todo = todo.json()
+            done_tasks = 0
             list_done_tasks = []
 
-            for i in range(num_total_tasks):
-                if response_todo[i]['completed'] is True:
-                    num_done_tasks += 1
-                    list_done_tasks.append(response_todo[i]['title'])
+            for i in range(len(todo)):
+                if todo[i]['completed'] is True:
+                    done_tasks += 1
+                    list_done_tasks.append(todo[i]['title'])
             print(f"Employee {user_name} is done with tasks"
-                  + f"({num_done_tasks}/{num_total_tasks}):")
+                  + f"({done_tasks}/{len(todo)}):")
             for task in list_done_tasks:
                 print(f"\t {task}")
